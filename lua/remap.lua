dcf11fe1cd0c83daa0e9af0a83885d62a5f0d9f6
Matt <mattfrayser@gmail.com>
2025-06-13 16:10:51 -0400

    add copilot and telescope

--- lua/remap.lua
+++ lua/remap.lua
@@ -31,3 +31,4 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 
 -- Make script executable from within script
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
